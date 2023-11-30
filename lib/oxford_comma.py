87bcc6ce905123003eb290a94967706ff5047554
livotieno <livio5261@gmail.com>
2023-11-30 13:49:07 +0300

    complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+    elif len(items) == 2: 
+        items.insert(-1, "and")
+        sentence = ' '.join(items)
+        return (sentence)
+    else:
+        last_item = items.pop()
+        items = [item + ',' for item in items]
+        items.append(last_item)
+        items.insert(-1, "and")
+        sentence = ' '.join(items)
+        return (sentence)
+
+        
+#oxford_comma(['Kiwi', 'Panda', 'Goat'])
